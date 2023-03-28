c298a35fa3ace53fd8817db682c7935487b3d6d4
ismahaneyy <liciousmayrah@gmail.com>
2023-03-28 12:56:59 +0300

    added the relationships and validations

--- task_manager/app/models/project.rb
+++ task_manager/app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :tasks 
+
+  validates :name, presence: true 
+  validates :duedate, presence: true 
+  
 end
--- task_manager/app/models/user.rb
+++ task_manager/app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    
+    has_secure_password
+    has_many :tasks
+    has_many :projects
+    has_many :password_reset 
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with:URI::MailTo::EMAIL_REGEXP}
+    validates :password, presence: true, length: { minimum:6 }, on::create
+
 end
