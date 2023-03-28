7c26fbe9d417e9089ec22fc948cf39aff6cc5872
Jeff <jeffgichuki68@gmail.com>
2023-03-28 14:21:07 +0300

    validations added

--- task_manager/app/models/task.rb
+++ task_manager/app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :completed, presence: true
+  validates :description, presence: true
+  validates :project_id, presence: true
+  validates :user_id, presence: true
+
+
 end
