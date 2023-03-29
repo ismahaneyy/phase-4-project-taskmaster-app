2dd85d935faf4a2bd4c3d4ea155de738293919c3
ismahaneyy <liciousmayrah@gmail.com>
2023-03-29 10:39:22 +0300

    added index method that gets all projects

--- task_manager/app/controllers/projects_controller.rb
+++ task_manager/app/controllers/projects_controller.rb
@@ -3,6 +3,11 @@ class ProjectsController < ApplicationController
     before_action :set_project, only: [:show, :update, :destroy]
     before_action :authenticate_user!
 
-    
+    def index
+        projects = current_user.projects
+        render json: projects
+      end
+  
+      
 
 end
