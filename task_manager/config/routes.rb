2d4b61755b0d401ce50c053632e47c22c4468b07
ismahaneyy <liciousmayrah@gmail.com>
2023-03-29 15:01:04 +0300

    updated the seed file and added the routes for project

--- task_manager/config/routes.rb
+++ task_manager/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:create, :update, :destroy]
+  resources :users, only: [:index, :create, :update, :destroy]
+  resources :projects, only: [:index, :create, :show, :update, :destroy]
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   post '/signup', to: 'users#create'
-  
+
 end
--- task_manager/db/seeds.rb
+++ task_manager/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#some users
+user_1 = User.create(name: "Jeff", email: "jeff@example.com", password: "1234567")
+user_2 = User.create(name: "Ismahan", email: "ismahan@example.com", password: "1234567")
+
+
+#some projects for each user
+user_1.projects.create(name: "First Project", description: "Jeff's first project", due_date: 1.week.from_now, completed: false)
+user_1.projects.create(name: "Second Project", description: "Jeff's second project", due_date: 2.weeks.from_now, completed: false)
+user_2.projects.create(name: "Third Project", description: "Ismahan's first project", due_date: 3.weeks.from_now, completed: false)
+user_2.projects.create(name: "Fourth Project", description: "Ismahan's second project", due_date: 4.weeks.from_now, completed: false)
