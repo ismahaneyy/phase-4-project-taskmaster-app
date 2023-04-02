268e6a9c9cb3d43758227c34a66e7e3454885d90
ismahaneyy <liciousmayrah@gmail.com>
2023-04-02 14:35:40 +0300

    replaced deliver_later to deliver_now

--- task_manager/app/controllers/password_reset_controller.rb
+++ task_manager/app/controllers/password_reset_controller.rb
@@ -6,7 +6,7 @@ class PasswordResetController < ApplicationController
 
         # send password reset email to user using PasswordResetMailer
 
-        PasswordResetMailer.password_reset(user).deliver_later
+        PasswordResetMailer.password_reset(user).deliver_now
         render json: { message:'Password reset instructions have been sent to your email.' } 
       else
         render json: { message:'Email address has not been found' } 
