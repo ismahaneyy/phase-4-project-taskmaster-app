349cdaa3a15af3243e15e08d371844e28b8f2b5b
ismahaneyy <liciousmayrah@gmail.com>
2023-04-02 12:56:49 +0300

    resolved merge conflicts

--- task_manager/app/controllers/password_reset_controller.rb
+++ task_manager/app/controllers/password_reset_controller.rb
@@ -3,14 +3,10 @@ class PasswordResetController < ApplicationController
     def new
       user = User.find_by_email(params[:email])
       if user
-<<<<<<< HEAD
-        PasswordResetMailer.password_reset(user).deliver_later
-=======
 
         # send password reset email to user using PasswordResetMailer
 
         PasswordResetMailer.password_reset(user).deliver_now
->>>>>>> 3238c28d272769360a54d97f7d344f0d6fafbd6d
         render json: { message:'Password reset instructions have been sent to your email.' } 
       else
         render json: { message:'Email address has not been found' } 
